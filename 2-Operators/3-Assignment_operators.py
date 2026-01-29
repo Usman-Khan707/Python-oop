733e34a8b63d4b5fa539992a6661bfe5d896c394
usman khan <musmankhan707@gmail.com>
2026-01-29 20:18:16 +0500

    logical operators done

---
+++ 2-Operators/3-Assignment_operators.py
@@ -0,0 +1,12 @@
+a = 10
+b = a # assignment operator
+
+print(b)
+a += 5 # a = a + 5
+print(a) 
+a += 5 # a = a - 5
+print(a)
+a *= 5 # a = a * 5 
+print(a) 
+a /= 5 # a = a / 5
+print(a) 
---
+++ 2-Operators/4-Logical_operators.py
@@ -0,0 +1,7 @@
+a = 5
+b = 10
+c = 5
+
+print(not (a == c))  
+print(a==c and b > a)
+print(a==c or b < a)
